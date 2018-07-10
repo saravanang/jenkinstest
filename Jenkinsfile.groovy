1c4ce906a213de4cc8549bae74934e0e29cb0716
saravanang <saravanang.g@gmail.com>
2018-07-09 23:33:52 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,12 @@
 node(){
   
-  
+  stage "checkout"
+  stage "compile"
+  stage "code analysis"
+  stage "package"
+  stage "upload"
+  stage "download"
+  stage "deploy"
+  stage "restart"
+  stage "finish"
 }
